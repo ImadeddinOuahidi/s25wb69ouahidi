6a49c9ed296a5c94b5e78a27b3d66f8759b9b71c
ImadeddinOuahidi <ouahidiimad@gmail.com>
2025-03-31 13:05:27 -0500

    updated insects

--- routes/insects.js
+++ routes/insects.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('insects', { title: 'Search Results Insects' });
+    let results = [
+        { name: "Monarch Butterfly", size: 10, lifespan: 6 },
+        { name: "Honey Bee", size: 3, lifespan: 5 },
+        { name: "Atlas Moth", size: 25, lifespan: 2 }
+      ];
+  res.render('insects', { title: 'Search Results Insects', results: results });
 });
 
 module.exports = router;
