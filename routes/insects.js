f465822d7e75b00a37ff5448d1218485d882d626
ImadeddinOuahidi <ouahidiimad@gmail.com>
2025-03-31 13:17:27 -0500

    added view and route for grid

--- routes/insects.js
+++ routes/insects.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     let results = [
         { name: "Monarch Butterfly", size: 10, lifespan: 6 },
         { name: "Honey Bee", size: 3, lifespan: 5 },
         { name: "Atlas Moth", size: 25, lifespan: 2 }
-      ];
-  res.render('insects', { title: 'Search Results Insects', results: results });
+    ];
+    res.render('insects', { title: 'Search Results Insects', results: results });
 });
 
 module.exports = router;
